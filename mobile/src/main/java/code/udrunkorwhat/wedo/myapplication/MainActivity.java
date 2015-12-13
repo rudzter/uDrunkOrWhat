45775255b86b761085298f1e00bfa7e8737a15f9
mmm320 <jorgre@hotmail.com>
2015-12-13 23:23:51 +0100

    added correct nav item id in MainActivity onNavigationItemSelected

--- mobile/src/main/java/code/udrunkorwhat/wedo/myapplication/MainActivity.java
+++ mobile/src/main/java/code/udrunkorwhat/wedo/myapplication/MainActivity.java
@@ -82,9 +82,9 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_drink) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_statistics) {
 
         } else if (id == R.id.nav_manage) {
 
